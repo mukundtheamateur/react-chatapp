753e9560f728cdbe946d57072b64859c31e7adf8
mukundtheamateur <mukundshukla000@gmail.com>
2023-04-28 15:48:20 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import './App.css';
+import Message from "./components/Message";
+import {Box, Button, Container, HStack, Input, VStack} from "@chakra-ui/react";
+import {getAuth,GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {app} from "./firebase";
+import { useState } from 'react';
+const auth = getAuth(app);
 
+const loginhandler = () =>{
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider);
+}
 function App() {
+  const [user, setUser] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box bg={"red.50"}>
+      <Container bg={"white"} h={"100vh"}>
+        <VStack h="full" paddingY="4 ">
+          <Button colorScheme={"red"} w={"full"}>
+            Logout
+          </Button>
+
+          <VStack h="full" w={"full"} overflowY="auto">
+           
+          </VStack>
+
+          <form style={{width: "100%"}}>
+          <HStack>
+            <Input placeholder="Enter a message..." />
+            
+            <Button colorScheme={"purple"} type="submit">Send</Button>
+
+            </HStack>
+          </form>
+        </VStack>
+      </Container>
+    </Box>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCxYkep-XkWekaCcEZAT32RKxtm__8Wxp0",
+  authDomain: "chat-app-20542.firebaseapp.com",
+  projectId: "chat-app-20542",
+  storageBucket: "chat-app-20542.appspot.com",
+  messagingSenderId: "93629212519",
+  appId: "1:93629212519:web:acee83c3ada276127a71c2",
+  measurementId: "G-NPGB2JNYEC"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
